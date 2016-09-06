d055ce29b959cd60f5b0769ed8222ab46c17fcd4
Tiago Neves <tiago.neves@vtex.com.br>
2016-09-06 20:35:26 -0300

    fix #5

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 var env = require('yargs').argv.mode;
+var extend = require('extend');
 
 var libraryName = 'splunk-events';
 
@@ -45,4 +46,33 @@ var config = {
   plugins: plugins
 };
 
-module.exports = config;
+var nodeConfig = extend(true, {}, config, {
+  target: 'node',
+  output: {
+    path: __dirname + '/dist-node',
+    filename: outputFile
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /(\.json)$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+});
+
+var configs = [nodeConfig, config];
+
+module.exports = configs;
